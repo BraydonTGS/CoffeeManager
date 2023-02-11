61f54909134a61a37b2079a6895469edf5483b77
Braydon Sutherland <BraydonSutherland@gmail.com>
2023-02-11 07:37:14 -0600

    Started Building out the CoffeeEquipmentPage

--- MyCoffeeApp.Mobile/MyCoffeeApp.Mobile/ViewModels/CoffeeEquipmentViewModel.cs
+++ MyCoffeeApp.Mobile/MyCoffeeApp.Mobile/ViewModels/CoffeeEquipmentViewModel.cs
@@ -23,7 +23,7 @@ namespace MyCoffeeApp.Mobile.ViewModels
             Coffee = new ObservableRangeCollection<Coffee>();
             CoffeeGroups = new ObservableRangeCollection<Grouping<string, Coffee>>();
 
-            var image = "https://images.prismic.io/yesplz/75c1e42d-4bcc-40e1-abec-bf35816c088b_Group+2471.png?auto=compress,format&rect=0,0,870,1341&w=870&h=1341"
+            var image = "https://images.prismic.io/yesplz/75c1e42d-4bcc-40e1-abec-bf35816c088b_Group+2471.png?auto=compress,format&rect=0,0,870,1341&w=870&h=1341";
             Coffee.Add(new Models.Coffee { Roaster = "Yes Plz", Name = "Sip of Sunshine", Image= image });
             Coffee.Add(new Models.Coffee { Roaster = "Blue Bottle", Name = "Feeling Blue", Image = image });
             Coffee.Add(new Models.Coffee { Roaster = "Cotume", Name = "Rags and Rain", Image = image });
