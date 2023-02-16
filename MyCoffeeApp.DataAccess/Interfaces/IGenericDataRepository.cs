b7eaaecb586fa2f7e84ee4951c1cac1dbbb1f05c
Braydon Sutherland <BraydonSutherland@gmail.com>
2023-02-16 17:09:55 -0600

    Created a Generic Repository for my data access

---
+++ MyCoffeeApp.DataAccess/Interfaces/IGenericDataRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace MyCoffeeApp.DataAccess.Interfaces
+{
+    public interface IGenericDataRepository<T>
+    {
+        public Task<IEnumerable<T>> GetAllAsync();
+        public Task<T> GetByIdAsync(Guid Id);
+        public Task<T> CreateAsync(T args);
+        public Task<T> UpdateAsync(Guid id, T args);
+        public Task<bool> DeleteAsync(int id);
+    }
+}
---
+++ MyCoffeeApp.DataAccess/Repository/GenericDataRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyCoffeeApp.DataAccess.Context;
+using MyCoffeeApp.DataAccess.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCoffeeApp.DataAccess.Repository
+{
+    public class GenericDataRepository<T> : IGenericDataRepository<T> where T : class
+    {
+ 
+        public GenericDataRepository(CoffeeDbContext context)
+        {
+            
+        }
+        public async Task<T> CreateAsync(T args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<T> GetByIdAsync(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<T> UpdateAsync(Guid id, T args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
