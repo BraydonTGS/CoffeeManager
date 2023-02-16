db0558cbbad84671ac6a010ebf7e16dc761f0bce
Braydon Sutherland <BraydonSutherland@gmail.com>
2023-02-16 17:13:10 -0600

    Created a DbContextFactory

---
+++ MyCoffeeApp.DataAccess/Context/DbContextFactory.cs
@@ -0,0 +1,10 @@
+﻿namespace MyCoffeeApp.DataAccess.Context
+{
+    public class DbContextFactory
+    {
+        public virtual CoffeeDbContext CreateDbContext()
+        {
+            return new CoffeeDbContext();
+        }
+    }
+}
