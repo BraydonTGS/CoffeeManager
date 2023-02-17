d0c40e3cd954ad384ee49a35ca1e1bf101d92ed3
Braydon Sutherland <BraydonSutherland@gmail.com>
2023-02-17 06:43:30 -0600

    Created DbContextFactory

---
+++ MyCoffeeApp.DataAccess/Context/CoffeeDbContextFactory.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyCoffeeApp.DataAccess.Context
+{
+    public class CoffeeDbContextFactory : IDbContextFactory<CoffeeDbContext>
+    {
+        public CoffeeDbContext CreateDbContext()
+        {
+            return new CoffeeDbContext();
+        }
+    }
+}
