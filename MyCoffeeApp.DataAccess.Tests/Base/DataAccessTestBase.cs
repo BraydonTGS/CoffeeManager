6e82d56c4c9057a9632225ed0f79ebfcca6797aa
Braydon Sutherland <BraydonSutherland@gmail.com>
2023-02-23 20:53:21 -0600

    Made the SeedContextWithMockData Abstract
    Imlemented by each test class to define the context that is tested

--- MyCoffeeApp.DataAccess.Tests/Base/DataAccessTestBase.cs
+++ MyCoffeeApp.DataAccess.Tests/Base/DataAccessTestBase.cs
@@ -8,7 +8,7 @@ using MyCoffeeApp.DataAccess.Repository;
 namespace MyCoffeeApp.DataAccess.Tests.Base
 {
     [TestClass]
-    public class DataAccessTestBase
+    public abstract class DataAccessTestBase
     {
         protected CoffeeDbContextFactory dbContextFactory;
         protected CoffeeDbContext dbContext;
@@ -24,72 +24,6 @@ namespace MyCoffeeApp.DataAccess.Tests.Base
             mockFactory.Setup(x => x.CreateDbContext()).Returns(mockDbContext);
             genericDataRepository = new GenericDataRepository<Coffee>(mockFactory.Object);
         }
-
-        #region GenerateMockData
-        private void SeedContextWithMockData()
-        {
-            if (mockDbContext != null)
-            {
-                var coffees = GenerateMockCoffees();
-                var user = GenerateMockUser();
-                user.Coffees = coffees;
-                mockDbContext.Users.Add(user);
-                foreach (var coffee in coffees)
-                {
-                    mockDbContext.Coffees.Add(coffee);
-                }
-                mockDbContext.SaveChanges();
-            }
-        }
-
-        private List<Coffee> GenerateMockCoffees()
-        {
-            var coffeeList = new List<Coffee>()
-            {
-               new Coffee()
-               {
-                CoffeeId = Guid.NewGuid(),
-                CoffeeName = "Toasted Hazelnut",
-                CoffeeRoaster = "Community Coffee",
-                ImagePath = "https://images.prismic.io/yesplz/75c1e42d-4bcc-40e1-abec-bf35816c088b_Group+2471.png?auto=compress,format&rect=0,0,870,1341&w=870&h=1341",
-                UserId = new Guid("67d28458-b179-48a1-bf96-71d9140a14ad")
-                },
-               new Coffee()
-               {
-                CoffeeId = Guid.NewGuid(),
-                CoffeeName = "Morning Blend",
-                CoffeeRoaster = "Trader Joes",
-                ImagePath = "https://images.prismic.io/yesplz/75c1e42d-4bcc-40e1-abec-bf35816c088b_Group+2471.png?auto=compress,format&rect=0,0,870,1341&w=870&h=1341",
-                UserId = new Guid("67d28458-b179-48a1-bf96-71d9140a14ad")
-                },
-                new Coffee()
-                {
-                CoffeeId = Guid.NewGuid(),
-                CoffeeName = "Vista",
-                CoffeeRoaster = "Counter Culture",
-                ImagePath = "https://images.prismic.io/yesplz/75c1e42d-4bcc-40e1-abec-bf35816c088b_Group+2471.png?auto=compress,format&rect=0,0,870,1341&w=870&h=1341",
-                UserId = new Guid("67d28458-b179-48a1-bf96-71d9140a14ad")
-                }
-            };
-
-            return coffeeList;
-        }
-
-        private User GenerateMockUser()
-        {
-            var user = new User()
-            {
-                UserId = new Guid("67d28458-b179-48a1-bf96-71d9140a14ad"),
-                FirstName = "Braydon",
-                LastName = "Sutherland",
-                Email = "BraydonSutherland@gmail.com",
-                Password = "TryAndGuess",
-                Username = "Geo"
-
-            };
-
-            return user;
-        }
-        #endregion
+        protected abstract void SeedContextWithMockData();
     }
 }
--- MyCoffeeApp.DataAccess.Tests/Tests/CoffeeRepositoryShould.cs
+++ MyCoffeeApp.DataAccess.Tests/Tests/CoffeeRepositoryShould.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyCoffeeApp.DataAccess.Entities;
 using MyCoffeeApp.DataAccess.Tests.Base;
+using NuGet.Frameworks;
 
 namespace MyCoffeeApp.DataAccess.Tests.Tests
 {
@@ -8,14 +10,14 @@ namespace MyCoffeeApp.DataAccess.Tests.Tests
     {
 
         [TestMethod]
-        public async Task ReturnCoffeesInCollectionNotNullAsync()
+        public async Task ReturnCoffeesInCollection_NotReturnNullAsync()
         {
             var sut = await genericDataRepository.GetAllAsync();
             Assert.IsNotNull(sut);
         }
 
         [TestMethod]
-        public async Task ReturnAllCoffeesInCollectionAsync()
+        public async Task ReturnAllCoffeesInCollection_Async()
         {
             var sut = await genericDataRepository.GetAllAsync();
             Assert.IsNotNull(sut);
@@ -23,7 +25,7 @@ namespace MyCoffeeApp.DataAccess.Tests.Tests
         }
 
         [TestMethod]
-        public async Task ReturnAllCoffeesInCollection_CheckFirstCoffee()
+        public async Task ReturnAllCoffeesInCollection_CheckFirstCoffeeAsync()
         {
             var sut = await genericDataRepository.GetAllAsync();
             var coffee = sut.First();
@@ -35,5 +37,87 @@ namespace MyCoffeeApp.DataAccess.Tests.Tests
             Assert.AreEqual(new Guid("67d28458-b179-48a1-bf96-71d9140a14ad"), coffee.UserId);
         }
 
+        [TestMethod]
+        public async Task GetCoffeeByGuid_AndNotReturnNullAsync()
+        {
+            var sut = await genericDataRepository.GetByIdAsync(new Guid("7d187a7f-d717-45f3-ba15-3bd20b84fba2"));
+            Assert.IsNotNull(sut);
+        }
+
+        [TestMethod]
+        public async Task GetCoffeeByGuid_AndCheckInputAsync()
+        {
+            var sut = await genericDataRepository.GetByIdAsync(new Guid("7d187a7f-d717-45f3-ba15-3bd20b84fba2"));
+            Assert.IsNotNull (sut);
+            Assert.AreEqual("Toasted Hazelnut", sut.CoffeeName);
+            Assert.AreEqual("Community Coffee", sut.CoffeeRoaster);
+            Assert.AreEqual("https://images.prismic.io/yesplz/75c1e42d-4bcc-40e1-abec-bf35816c088b_Group+2471.png?auto=compress,format&rect=0,0,870,1341&w=870&h=1341", sut.ImagePath);
+            Assert.AreEqual(new Guid("67d28458-b179-48a1-bf96-71d9140a14ad"), sut.UserId);
+        }
+
+        #region SeedContextWithMockData
+        protected override void SeedContextWithMockData()
+        {
+            if (mockDbContext != null)
+            {
+                var coffees = GenerateMockCoffees();
+                var user = GenerateMockUser();
+                user.Coffees = coffees;
+                mockDbContext.Users.Add(user);
+                foreach (var coffee in coffees)
+                {
+                    mockDbContext.Coffees.Add(coffee);
+                }
+                mockDbContext.SaveChanges();
+            }
+        }
+        private List<Coffee> GenerateMockCoffees()
+        {
+            var coffeeList = new List<Coffee>()
+            {
+               new Coffee()
+               {
+                CoffeeId = new Guid("7d187a7f-d717-45f3-ba15-3bd20b84fba2"),
+                CoffeeName = "Toasted Hazelnut",
+                CoffeeRoaster = "Community Coffee",
+                ImagePath = "https://images.prismic.io/yesplz/75c1e42d-4bcc-40e1-abec-bf35816c088b_Group+2471.png?auto=compress,format&rect=0,0,870,1341&w=870&h=1341",
+                UserId = new Guid("67d28458-b179-48a1-bf96-71d9140a14ad")
+                },
+               new Coffee()
+               {
+                CoffeeId = Guid.NewGuid(),
+                CoffeeName = "Morning Blend",
+                CoffeeRoaster = "Trader Joes",
+                ImagePath = "https://images.prismic.io/yesplz/75c1e42d-4bcc-40e1-abec-bf35816c088b_Group+2471.png?auto=compress,format&rect=0,0,870,1341&w=870&h=1341",
+                UserId = new Guid("67d28458-b179-48a1-bf96-71d9140a14ad")
+                },
+                new Coffee()
+                {
+                CoffeeId = Guid.NewGuid(),
+                CoffeeName = "Vista",
+                CoffeeRoaster = "Counter Culture",
+                ImagePath = "https://images.prismic.io/yesplz/75c1e42d-4bcc-40e1-abec-bf35816c088b_Group+2471.png?auto=compress,format&rect=0,0,870,1341&w=870&h=1341",
+                UserId = new Guid("67d28458-b179-48a1-bf96-71d9140a14ad")
+                }
+            };
+
+            return coffeeList;
+        }
+        private User GenerateMockUser()
+        {
+            var user = new User()
+            {
+                UserId = new Guid("67d28458-b179-48a1-bf96-71d9140a14ad"),
+                FirstName = "Braydon",
+                LastName = "Sutherland",
+                Email = "BraydonSutherland@gmail.com",
+                Password = "TryAndGuess",
+                Username = "Geo"
+
+            };
+
+            return user;
+        }
+        #endregion
     }
 }
