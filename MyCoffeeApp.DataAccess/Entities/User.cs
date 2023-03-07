0db0f8ecb35087a2e2afae4cad2abb297586da38
Braydon Sutherland <BraydonSutherland@gmail.com>
2023-03-06 21:21:09 -0600

    Updated DataAccess Entities

--- MyCoffeeApp.DataAccess/Entities/User.cs
+++ MyCoffeeApp.DataAccess/Entities/User.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MyCoffeeApp.DataAccess.Entities
@@ -26,7 +27,6 @@ namespace MyCoffeeApp.DataAccess.Entities
 
         [Required]
         public string Password { get; set; } = string.Empty;
-
         public IEnumerable<Coffee> Coffees { get; set; } = Enumerable.Empty<Coffee>();
     }
 }
